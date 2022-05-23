4f190b128ef36b471c0dc5d2a5d89de48ae3f6bb
armimeira <meiradewantari@gmail.com>
2022-05-23 15:31:29 +0800

    meira update route

--- routes/web.php
+++ routes/web.php
@@ -3,17 +3,10 @@
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\ContactController;
-use App\Http\Controllers\CourierController;
-use App\Http\Controllers\ProductController;
-use App\Http\Controllers\DiscountController;
 use App\Http\Controllers\HomeBaseController;
 use App\Http\Controllers\LoginController;
-use App\Http\Controllers\ProductImageController;
-use App\Http\Controllers\ProductReviewController;
 use App\Http\Controllers\ChangePasswordController;
 use App\Http\Controllers\CartController;
-use App\Http\Controllers\ProductCategoryController;
-use App\Http\Controllers\ProductCategoryDetailController;
 use App\Http\Controllers\AdminController;
 use App\Models\product_review;
 
